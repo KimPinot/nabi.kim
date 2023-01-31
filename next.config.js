f7b9c9375f74aae32b998534c437a1659f7ba4b6
nabi.lee <hello@nabi.kim>
2023-02-01 01:36:09 +0900

    Add CV redirect

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const config = {
   experimental: {
     appDir: true,
   },
+  redirects: async () => [
+    {
+      source: "/cv",
+      destination:
+        "https://nane.notion.site/CV-4779ec94b3f24a909fb9c2afbfcba343",
+      permanent: true,
+    },
+  ],
 };
 
 module.exports = config;
