d4d6a3952b822f957295bea48b83ccaf955efb8d
nabi.lee <hello@nabi.kim>
2023-02-03 21:04:45 +0900

    Fix sitemap generator schema

--- scripts/sitemap.cjs
+++ scripts/sitemap.cjs
@@ -26,12 +26,7 @@ const sitemap = (articles) => {
   };
 
   return [
-    `<?xml version="1.0" encoding="UTF-8"?>`,
-    `<urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-        http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`,
+    `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">`,
     url("/", 1),
     url("/about.md", 0.8),
     url("/category", 0.8),
